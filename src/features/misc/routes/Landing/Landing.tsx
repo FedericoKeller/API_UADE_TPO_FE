316f0195d6e875578e7b345509d75768ce7c2a7d
Federico Keller <federicokeller27@gmail.com>
2024-04-05 18:41:47 -0300

    feature: created FilmCard component

--- src/features/misc/routes/Landing/Landing.tsx
+++ src/features/misc/routes/Landing/Landing.tsx
@@ -1,10 +1,13 @@
 import { DoubleHeader } from "../../components/DoubleHeader";
+import { FilmCard } from "../../components/FilmCard";
+import { Container } from '@mantine/core';
 
+import './Landing.scss';
 export const Landing = () => {
   return (
-    <>
+    <Container>
       <DoubleHeader />
-      Hello world!
-    </>
+      <FilmCard />
+    </Container>
   );
 };

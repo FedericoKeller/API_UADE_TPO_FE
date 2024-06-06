39710b46dcc526bf9e1079f04969be4a0f1bae34
Federico Keller <federicokeller27@gmail.com>
2024-06-05 22:25:59 -0300

    feat: added toast to activate account

--- src/features/auth/routes/Register.tsx
+++ src/features/auth/routes/Register.tsx
@@ -2,19 +2,27 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { Layout } from '../components/Layout';
 import { RegisterForm } from '../components/RegisterForm/RegisterForm';
+import { notifications } from '@mantine/notifications';
 
 export const Register = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const redirectTo = searchParams.get('redirectTo');
 
+  const onSuccess = () => {
+    notifications.show({
+      color: "green",
+      message: "¡Cuenta creada! Revisa tu mail para activar tu cuenta",
+    });
+
+    navigate(`${redirectTo ? `${redirectTo}` : '/app'}`, {
+      replace: true,
+    })
+  }
+
   return (
     <Layout title="Register">
-      <RegisterForm onSuccess={() =>
-          navigate(`${redirectTo ? `${redirectTo}` : '/app'}`, {
-            replace: true,
-          })
-        } />
+      <RegisterForm onSuccess={onSuccess} />
     </Layout>
   );
 };
